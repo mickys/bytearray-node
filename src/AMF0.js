86be4f7c75057983001c6f989fff6c9954d247e2
Daan <foifhjidhf@xd.com>
2018-10-22 17:14:21 +0200

    V1.2.9

--- src/AMF0.js
+++ src/AMF0.js
@@ -1,187 +1,187 @@
 "use strict"
 
 class AMF0 {
-	constructor(ba) {
-		this.ba = ba
-		this.references = new Map()
-	}
+  constructor(ba) {
+    this.ba = ba
+    this.references = new Map()
+  }
 
-	isStrict(array) {
-		return Object.keys(array).reduce((isStrict, key) => isStrict && Number.isInteger(key), true)
-	}
+  isStrict(array) {
+    return Object.keys(array).reduce((isStrict, key) => isStrict && Number.isInteger(key), true)
+  }
 
-	writeData(val) {
-		if (val === null) {
-			this.ba.writeByte(5)
-		} else if (val === undefined) {
-			this.ba.writeByte(6)
-		} else if (typeof val === "boolean") {
-			this.ba.writeByte(1)
-			this.ba.writeBoolean(val)
-		} else if (typeof val === "number") {
-			this.ba.writeByte(0)
-			this.ba.writeDouble(val)
-		} else if (val instanceof Date) {
-			this.ba.writeByte(11)
-			this.ba.writeDouble(val.getTime())
-		} else if (typeof val === "string") {
-			this.ba.writeByte(2)
-			this.writeString(val)
-		} else if (val instanceof Array) {
-			if (this.references.has(val)) {
-				return this.writeReference(val)
-			}
+  writeData(val) {
+    if (val === null) {
+      this.ba.writeByte(5)
+    } else if (val === undefined) {
+      this.ba.writeByte(6)
+    } else if (typeof val === "boolean") {
+      this.ba.writeByte(1)
+      this.ba.writeBoolean(val)
+    } else if (typeof val === "number") {
+      this.ba.writeByte(0)
+      this.ba.writeDouble(val)
+    } else if (val instanceof Date) {
+      this.ba.writeByte(11)
+      this.ba.writeDouble(val.getTime())
+    } else if (typeof val === "string") {
+      this.ba.writeByte(2)
+      this.writeString(val)
+    } else if (val instanceof Array) {
+      if (this.references.has(val)) {
+        return this.writeReference(val)
+      }
 
-			this.references.set(val, this.references.size)
+      this.references.set(val, this.references.size)
 
-			if (this.isStrict(val)) {
-				this.ba.writeByte(10)
-				this.ba.writeUnsignedInt(val.length)
+      if (this.isStrict(val)) {
+        this.ba.writeByte(10)
+        this.ba.writeUnsignedInt(val.length)
 
-				for (const element of val) {
-					this.writeData(element)
-				}
-			} else {
-				this.ba.writeByte(8)
-				this.ba.writeUnsignedInt(val.length)
+        for (const element of val) {
+          this.writeData(element)
+        }
+      } else {
+        this.ba.writeByte(8)
+        this.ba.writeUnsignedInt(val.length)
 
-				for (const key in val) {
-					this.writeString(key)
-					this.writeData(val[key])
-				}
+        for (const key in val) {
+          this.writeString(key)
+          this.writeData(val[key])
+        }
 
-				this.ba.writeShort(0)
-				this.ba.writeByte(9)
-			}
-		} else if (val instanceof Object) {
-			if (this.references.has(val)) {
-				return this.writeReference(val)
-			}
+        this.ba.writeShort(0)
+        this.ba.writeByte(9)
+      }
+    } else if (val instanceof Object) {
+      if (this.references.has(val)) {
+        return this.writeReference(val)
+      }
 
-			this.references.set(val, this.references.size)
+      this.references.set(val, this.references.size)
 
-			if (val["@name"] !== undefined) {
-				this.ba.writeByte(16)
-				this.writeString(val["@name"])
-			} else {
-				this.ba.writeByte(3)
-			}
+      if (val["@name"] !== undefined) {
+        this.ba.writeByte(16)
+        this.writeString(val["@name"])
+      } else {
+        this.ba.writeByte(3)
+      }
 
-			for (const key in val) {
-				if (key[0] !== "@") {
-					this.writeString(key)
-					this.writeData(val[key])
-				}
-			}
+      for (const key in val) {
+        if (key[0] !== "@") {
+          this.writeString(key)
+          this.writeData(val[key])
+        }
+      }
 
-			this.ba.writeShort(0)
-			this.ba.writeByte(9)
-		} else {
-			throw new TypeError(`Unknown type: ${typeof val}`)
-		}
-	}
+      this.ba.writeShort(0)
+      this.ba.writeByte(9)
+    } else {
+      throw new TypeError(`Unknown type: ${typeof val}`)
+    }
+  }
 
-	readData() {
-		const marker = this.ba.readByte()
+  readData() {
+    const marker = this.ba.readByte()
 
-		if (marker === 5) {
-			return null
-		} else if (marker === 6 || marker === 13) {
-			return undefined
-		} else if (marker === 1) {
-			return this.ba.readBoolean()
-		} else if (marker === 0) {
-			return this.ba.readDouble()
-		} else if (marker === 11) {
-			return new Date(this.ba.readDouble()).toString()
-		} else if (marker === 2) {
-			return this.ba.readUTF()
-		} else if (marker === 12 || marker === 15) {
-			return this.ba.readUTFBytes(this.ba.readUnsignedInt())
-		} else if (marker === 10) {
-			const val = []
+    if (marker === 5) {
+      return null
+    } else if (marker === 6 || marker === 13) {
+      return undefined
+    } else if (marker === 1) {
+      return this.ba.readBoolean()
+    } else if (marker === 0) {
+      return this.ba.readDouble()
+    } else if (marker === 11) {
+      return new Date(this.ba.readDouble()).toString()
+    } else if (marker === 2) {
+      return this.ba.readUTF()
+    } else if (marker === 12 || marker === 15) {
+      return this.ba.readUTFBytes(this.ba.readUnsignedInt())
+    } else if (marker === 10) {
+      const val = []
 
-			this.references.set(this.references.size, val)
+      this.references.set(this.references.size, val)
 
-			const len = this.ba.readUnsignedInt()
+      const len = this.ba.readUnsignedInt()
 
-			for (let i = 0; i < len; i++) {
-				val.push(this.readData())
-			}
+      for (let i = 0; i < len; i++) {
+        val.push(this.readData())
+      }
 
-			return val
-		} else if (marker === 3) {
-			const val = {}
+      return val
+    } else if (marker === 3) {
+      const val = {}
 
-			this.references.set(this.references.size, val)
+      this.references.set(this.references.size, val)
 
-			for (let key = this.ba.readUTF(); key !== ""; key = this.ba.readUTF()) {
-				if (key[0] !== "@") {
-					val[key] = this.readData()
-				}
-			}
+      for (let key = this.ba.readUTF(); key !== ""; key = this.ba.readUTF()) {
+        if (key[0] !== "@") {
+          val[key] = this.readData()
+        }
+      }
 
-			if (this.ba.readByte() === 9) {
-				return val
-			}
-		} else if (marker === 16) {
-			const val = {}
+      if (this.ba.readByte() === 9) {
+        return val
+      }
+    } else if (marker === 16) {
+      const val = {}
 
-			this.references.set(this.references.size, val)
+      this.references.set(this.references.size, val)
 
-			val["@name"] = this.ba.readUTF()
+      val["@name"] = this.ba.readUTF()
 
-			for (let key = this.ba.readUTF(); key !== ""; key = this.ba.readUTF()) {
-				if (key[0] !== "@") {
-					val[key] = this.readData()
-				}
-			}
+      for (let key = this.ba.readUTF(); key !== ""; key = this.ba.readUTF()) {
+        if (key[0] !== "@") {
+          val[key] = this.readData()
+        }
+      }
 
-			if (this.ba.readByte() === 9) {
-				return val
-			}
-		} else if (marker === 7) {
-			return this.references.get(this.ba.readUnsignedShort())
-		} else if (marker === 8) {
-			const val = []
-			const len = this.ba.readUnsignedInt()
-			let elementName = this.ba.readUTF()
+      if (this.ba.readByte() === 9) {
+        return val
+      }
+    } else if (marker === 7) {
+      return this.references.get(this.ba.readUnsignedShort())
+    } else if (marker === 8) {
+      const val = []
+      const len = this.ba.readUnsignedInt()
+      let elementName = this.ba.readUTF()
 
-			while (elementName.length > 0 && len !== 0) {
-				val[elementName] = this.readData()
-				elementName = this.ba.readUTF()
-			}
+      while (elementName.length > 0 && len !== 0) {
+        val[elementName] = this.readData()
+        elementName = this.ba.readUTF()
+      }
 
-			if (this.ba.readByte() === 9) {
-				return val
-			}
-		} else if (marker === 17) {
-			this.ba.objectEncoding = 3
+      if (this.ba.readByte() === 9) {
+        return val
+      }
+    } else if (marker === 17) {
+      this.ba.objectEncoding = 3
 
-			return this.ba.readObject()
-		} else {
-			throw new TypeError(`Unknown marker: ${marker}`)
-		}
-	}
+      return this.ba.readObject()
+    } else {
+      throw new TypeError(`Unknown marker: ${marker}`)
+    }
+  }
 
-	writeReference(val) {
-		const reference = this.references.get(val)
+  writeReference(val) {
+    const reference = this.references.get(val)
 
-		if (reference <= 0xFFFF) {
-			this.ba.writeByte(7)
-			this.ba.writeUnsignedShort(reference)
-		}
-	}
+    if (reference <= 0xffff) {
+      this.ba.writeByte(7)
+      this.ba.writeUnsignedShort(reference)
+    }
+  }
 
-	writeString(val) {
-		if (val.length <= 0xFFFF) {
-			return this.ba.writeUTF(val)
-		}
+  writeString(val) {
+    if (val.length <= 0xffff) {
+      return this.ba.writeUTF(val)
+    }
 
-		this.ba.writeByte(12)
-		this.ba.writeUnsignedInt(val.length)
-		this.ba.writeUTFBytes(val)
-	}
+    this.ba.writeByte(12)
+    this.ba.writeUnsignedInt(val.length)
+    this.ba.writeUTFBytes(val)
+  }
 }
 
 module.exports = AMF0
--- src/ByteArray.js
+++ src/ByteArray.js
@@ -9,401 +9,425 @@ const AMF0 = require("./AMF0")
 const DEFAULT_SIZE = 2048
 
 class ByteArray {
-	constructor(buffer) {
-		this.writePosition = 0
-		this.readPosition = 0
-		this.endian = true
-		this.objectEncoding = 3
-
-		if (buffer instanceof ByteArray) {
-			this.buffer = buffer.buffer
-		} else if (Buffer.isBuffer(buffer)) {
-			this.buffer = buffer
-		} else {
-			this.buffer = Buffer.alloc(typeof(buffer) === "number" ? parseInt(buffer) : DEFAULT_SIZE)
-		}
-	}
-
-	get bytesAvailable() {
-		return this.writePosition - this.readPosition
-	}
-
-	get length() {
-		return this.buffer.length
-	}
-
-	clear() {
-		this.buffer = Buffer.alloc(DEFAULT_SIZE)
-		this.reset()
-	}
-
-	reset() {
-		this.writePosition = 0
-		this.readPosition = 0
-	}
-
-	canWrite(length) {
-		return (this.length - this.writePosition) >= length
-	}
-
-	scaleBuffer(length) {
-		const oldBuffer = this.buffer
-
-		this.buffer = Buffer.alloc(this.buffer.length + DEFAULT_SIZE + length)
-
-		oldBuffer.copy(this.buffer)
-	}
-
-	atomicCompareAndSwapIntAt(byteIndex, expectedValue, newValue) {
-		let byte = this.buffer[byteIndex]
-
-		if (byte === expectedValue) {
-			this.buffer[byteIndex] = newValue
-		}
-
-		return byte
-	}
-
-	atomicCompareAndSwapLength(expectedLength, newLength) {
-		const prevLength = this.length
-
-		if (prevLength !== expectedLength) {
-			return prevLength
-		}
-
-		if (prevLength < newLength) {
-			this.buffer = Buffer.concat([this.buffer, Buffer.alloc(newLength - prevLength)], newLength)
-		}
-
-		if (prevLength > newLength) {
-			this.buffer = this.buffer.slice(newLength - 1, prevLength - 1)
-		}
-
-		return prevLength
-	}
-
-	compress(algorithm) {
-		algorithm = algorithm.toLowerCase()
-
-		if (algorithm === "zlib") {
-			this.buffer = zlib.deflateSync(this.buffer)
-		} else if (algorithm === "deflate") {
-			this.buffer = zlib.deflateRawSync(this.buffer)
-		} else if (algorithm === "lzma") {
-			lzma.LZMA().compress(this.buffer, 1, (res) => {
-				this.buffer = res
-			})
-		} else {
-			throw new Error("Unknown algorithm")
-		}
-	}
+  constructor(buffer) {
+    this.writePosition = 0
+    this.readPosition = 0
+    this.endian = true
+    this.objectEncoding = 3
+
+    if (buffer instanceof ByteArray) {
+      this.buffer = buffer.buffer
+    } else if (Buffer.isBuffer(buffer)) {
+      this.buffer = buffer
+    } else {
+      this.buffer = Buffer.alloc(typeof buffer === "number" ? parseInt(buffer) : DEFAULT_SIZE)
+    }
+  }
+
+  get bytesAvailable() {
+    return this.writePosition - this.readPosition
+  }
+
+  get length() {
+    return this.buffer.length
+  }
+
+  clear() {
+    this.buffer = Buffer.alloc(DEFAULT_SIZE)
+    this.reset()
+  }
+
+  reset() {
+    this.writePosition = 0
+    this.readPosition = 0
+  }
+
+  canWrite(length) {
+    return this.length - this.writePosition >= length
+  }
+
+  scaleBuffer(length) {
+    const oldBuffer = this.buffer
+
+    this.buffer = Buffer.alloc(this.length + DEFAULT_SIZE + length)
+
+    oldBuffer.copy(this.buffer)
+  }
+
+  atomicCompareAndSwapIntAt(byteIndex, expectedValue, newValue) {
+    let byte = this.buffer[byteIndex]
+
+    if (byte === expectedValue) {
+      this.buffer[byteIndex] = newValue
+    }
+
+    return byte
+  }
+
+  atomicCompareAndSwapLength(expectedLength, newLength) {
+    const prevLength = this.length
+
+    if (prevLength !== expectedLength) {
+      return prevLength
+    }
+
+    if (prevLength < newLength) {
+      this.buffer = Buffer.concat([this.buffer, Buffer.alloc(newLength - prevLength)], newLength)
+    }
+
+    if (prevLength > newLength) {
+      this.buffer = this.buffer.slice(newLength - 1, prevLength - 1)
+    }
+
+    return prevLength
+  }
+
+  compress(algorithm) {
+    algorithm = algorithm.toLowerCase()
+
+    if (algorithm === "zlib") {
+      this.buffer = zlib.deflateSync(this.buffer)
+    } else if (algorithm === "deflate") {
+      this.buffer = zlib.deflateRawSync(this.buffer)
+    } else if (algorithm === "lzma") {
+      lzma.LZMA().compress(this.buffer, 1, (res) => {
+        this.buffer = res
+      })
+    } else {
+      throw new Error("Unknown algorithm")
+    }
+  }
+
+  deflate() {
+    this.compress("deflate")
+  }
+
+  inflate() {
+    this.uncompress("deflate")
+  }
+
+  uncompress(algorithm) {
+    algorithm = algorithm.toLowerCase()
+
+    if (algorithm === "zlib") {
+      this.buffer = zlib.inflateSync(this.buffer)
+    } else if (algorithm === "deflate") {
+      this.buffer = zlib.inflateRawSync(this.buffer)
+    } else if (algorithm === "lzma") {
+      lzma.LZMA().decompress(this.buffer, (res) => {
+        this.buffer = res
+      })
+    } else {
+      throw new Error("Unknown algorithm")
+    }
 
-	deflate() {
-		this.compress("deflate")
-	}
+    this.reset()
+  }
 
-	inflate() {
-		this.uncompress("deflate")
-	}
+  readBoolean() {
+    return this.readByte() !== 0
+  }
 
-	uncompress(algorithm) {
-		algorithm = algorithm.toLowerCase()
+  readByte() {
+    const value = this.buffer.readInt8(this.readPosition)
 
-		if (algorithm === "zlib") {
-			this.buffer = zlib.inflateSync(this.buffer)
-		} else if (algorithm === "deflate") {
-			this.buffer = zlib.inflateRawSync(this.buffer)
-		} else if (algorithm === "lzma") {
-			lzma.LZMA().decompress(this.buffer, (res) => {
-				this.buffer = res
-			})
-		} else {
-			throw new Error("Unknown algorithm")
-		}
+    this.readPosition += 1
 
-		this.reset()
-	}
+    return value
+  }
 
-	readBoolean() {
-		return this.readByte() !== 0
-	}
+  readBytes(buffer, offset = 0, length = 0) {
+    if (offset < 0 || length < 0) {
+      throw new RangeError("Offset/Length can't be less than 0")
+    }
 
-	readByte() {
-		const value = this.buffer.readInt8(this.readPosition)
+    if (length === 0) {
+      length = this.bytesAvailable
+    }
 
-		this.readPosition += 1
+    if (length > this.bytesAvailable) {
+      throw new RangeError("Length can't be greater than the bytes available")
+    }
 
-		return value
-	}
+    const total = offset + length
 
-	readBytes(buffer, offset = 0, length = 0) {
-		if (offset < 0 || length < 0) {
-			throw new RangeError("Offset/Length can't be less than 0")
-		}
+    if (total !== offset + length) {
+      throw new RangeError("32-bit overflow")
+    }
 
-		if (length === 0) {
-			length = this.bytesAvailable
-		}
+    if (!buffer.canWrite(offset + length)) {
+      buffer.scaleBuffer(offset + length)
+    }
 
-		if (length > this.bytesAvailable) {
-			throw new RangeError("Length can't be greater than the bytes available")
-		}
+    if (length > 0) {
+      for (let i = 0; i < length; i++) {
+        buffer.writeByte(this.readByte())
+      }
+    }
+  }
 
-		const total = offset + length
+  readDouble() {
+    const value = this.endian
+      ? this.buffer.readDoubleBE(this.readPosition)
+      : this.buffer.readDoubleLE(this.readPosition)
 
-		if (total !== (offset + length)) {
-			throw new RangeError("32-bit overflow")
-		}
+    this.readPosition += 8
 
-		if (!buffer.canWrite(offset + length)) {
-			buffer.scaleBuffer(offset + length)
-		}
+    return value
+  }
 
-		if (length > 0) {
-			for (let i = 0; i < length; i++) {
-				buffer.writeByte(this.readByte())
-			}
-		}
-	}
+  readFloat() {
+    const value = this.endian
+      ? this.buffer.readFloatBE(this.readPosition)
+      : this.buffer.readFloatLE(this.readPosition)
 
-	readDouble() {
-		const value = this.endian ? this.buffer.readDoubleBE(this.readPosition) : this.buffer.readDoubleLE(this.readPosition)
+    this.readPosition += 4
 
-		this.readPosition += 8
+    return value
+  }
 
-		return value
-	}
+  readInt() {
+    const value = this.endian
+      ? this.buffer.readInt32BE(this.readPosition)
+      : this.buffer.readInt32LE(this.readPosition)
 
-	readFloat() {
-		const value = this.endian ? this.buffer.readFloatBE(this.readPosition) : this.buffer.readFloatLE(this.readPosition)
+    this.readPosition += 4
 
-		this.readPosition += 4
+    return value
+  }
 
-		return value
-	}
+  readMultiByte(length, charSet = "utf8") {
+    const position = this.readPosition
 
-	readInt() {
-		const value = this.endian ? this.buffer.readInt32BE(this.readPosition) : this.buffer.readInt32LE(this.readPosition)
+    this.readPosition += length
 
-		this.readPosition += 4
+    if (Buffer.isEncoding(charSet)) {
+      return this.buffer.toString(charSet, position, position + length)
+    }
+  }
 
-		return value
-	}
+  readObject() {
+    return this.objectEncoding === 3 ? new AMF3(this).readData() : new AMF0(this).readData()
+  }
 
-	readMultiByte(length, charSet = "utf8") {
-		const position = this.readPosition
+  readShort() {
+    const value = this.endian
+      ? this.buffer.readInt16BE(this.readPosition)
+      : this.buffer.readInt16LE(this.readPosition)
 
-		this.readPosition += length
+    this.readPosition += 2
 
-		if (Buffer.isEncoding(charSet)) {
-			return this.buffer.toString(charSet, position, position + length)
-		}
-	}
+    return value
+  }
 
-	readObject() {
-		return this.objectEncoding === 3 ? new AMF3(this).readData() : new AMF0(this).readData()
-	}
+  readUnsignedByte() {
+    const value = this.buffer.readUInt8(this.readPosition)
 
-	readShort() {
-		const value = this.endian ? this.buffer.readInt16BE(this.readPosition) : this.buffer.readInt16LE(this.readPosition)
+    this.readPosition += 1
 
-		this.readPosition += 2
+    return value
+  }
 
-		return value
-	}
+  readUnsignedInt() {
+    const value = this.endian
+      ? this.buffer.readUInt32BE(this.readPosition)
+      : this.buffer.readUInt32LE(this.readPosition)
 
-	readUnsignedByte() {
-		const value = this.buffer.readUInt8(this.readPosition)
+    this.readPosition += 4
 
-		this.readPosition += 1
+    return value
+  }
 
-		return value
-	}
+  readUnsignedShort() {
+    const value = this.endian
+      ? this.buffer.readUInt16BE(this.readPosition)
+      : this.buffer.readUInt16LE(this.readPosition)
 
-	readUnsignedInt() {
-		const value = this.endian ? this.buffer.readUInt32BE(this.readPosition) : this.buffer.readUInt32LE(this.readPosition)
+    this.readPosition += 2
 
-		this.readPosition += 4
+    return value
+  }
 
-		return value
-	}
+  readUTF() {
+    const length = this.readShort()
+    const position = this.readPosition
 
-	readUnsignedShort() {
-		const value = this.endian ? this.buffer.readUInt16BE(this.readPosition) : this.buffer.readUInt16LE(this.readPosition)
+    this.readPosition += length
 
-		this.readPosition += 2
+    return this.buffer.toString("utf8", position, position + length)
+  }
 
-		return value
-	}
+  readUTFBytes(length) {
+    return this.readMultiByte(length)
+  }
 
-	readUTF() {
-		const length = this.readShort()
-		const position = this.readPosition
+  toJSON() {
+    return this.buffer.toJSON()
+  }
 
-		this.readPosition += length
+  toString(charSet = "utf8", offset = 0, length = this.length) {
+    return this.buffer.toString(charSet, offset, length)
+  }
 
-		return this.buffer.toString("utf8", position, position + length)
-	}
+  writeBoolean(value) {
+    this.writeByte(value ? 1 : 0)
+  }
 
-	readUTFBytes(length) {
-		return this.readMultiByte(length)
-	}
+  writeByte(value) {
+    if (!this.canWrite(1)) {
+      this.scaleBuffer(1)
+    }
 
-	toJSON() {
-		return this.buffer.toJSON()
-	}
+    this.buffer.writeInt8(value, this.writePosition)
 
-	toString(charSet = "utf8", offset = 0, length = this.length) {
-		return this.buffer.toString(charSet, offset, length)
-	}
+    this.writePosition += 1
+  }
 
-	writeBoolean(value) {
-		this.writeByte(value ? 1 : 0)
-	}
+  writeBytes(buffer, offset = 0, length = 0) {
+    if (offset < 0 || length < 0) {
+      throw new Error("Offset/Length can't be less than 0")
+    }
 
-	writeByte(value) {
-		if (!this.canWrite(1)) {
-			this.scaleBuffer(1)
-		}
+    if (offset > buffer.length) {
+      offset = buffer.length
+    }
 
-		this.buffer.writeInt8(value, this.writePosition)
+    if (length === 0) {
+      length = buffer.length - offset
+    }
 
-		this.writePosition += 1
-	}
+    if (length > buffer.length - offset) {
+      throw new RangeError("Length can't be greater than the buffer length")
+    }
 
-	writeBytes(buffer, offset = 0, length = 0) {
-		if (offset < 0 || length < 0) {
-			throw new Error("Offset/Length can't be less than 0")
-		}
+    if (length > 0) {
+      for (let i = offset; i < length; i++) {
+        this.writeByte(buffer.readByte())
+      }
+    }
+  }
 
-		if (offset > buffer.length) {
-			offset = buffer.length
-		}
+  writeDouble(value) {
+    if (!this.canWrite(8)) {
+      this.scaleBuffer(8)
+    }
 
-		if (length === 0) {
-			length = buffer.length - offset
-		}
+    this.endian
+      ? this.buffer.writeDoubleBE(value, this.writePosition)
+      : this.buffer.writeDoubleLE(value, this.writePosition)
 
-		if (length > buffer.length - offset) {
-			throw new RangeError("Length can't be greater than the buffer length")
-		}
+    this.writePosition += 8
+  }
 
-		if (length > 0) {
-			for (let i = offset; i < length; i++) {
-				this.writeByte(buffer.readByte())
-			}
-		}
-	}
+  writeFloat(value) {
+    if (!this.canWrite(4)) {
+      this.scaleBuffer(4)
+    }
 
-	writeDouble(value) {
-		if (!this.canWrite(8)) {
-			this.scaleBuffer(8)
-		}
+    this.endian
+      ? this.buffer.writeFloatBE(value, this.writePosition)
+      : this.buffer.writeFloatLE(value, this.writePosition)
 
-		this.endian ? this.buffer.writeDoubleBE(value, this.writePosition) : this.buffer.writeDoubleLE(value, this.writePosition)
+    this.writePosition += 4
+  }
 
-		this.writePosition += 8
-	}
+  writeInt(value) {
+    if (!this.canWrite(4)) {
+      this.scaleBuffer(4)
+    }
 
-	writeFloat(value) {
-		if (!this.canWrite(4)) {
-			this.scaleBuffer(4)
-		}
+    this.endian
+      ? this.buffer.writeInt32BE(value, this.writePosition)
+      : this.buffer.writeInt32LE(value, this.writePosition)
 
-		this.endian ? this.buffer.writeFloatBE(value, this.writePosition) : this.buffer.writeFloatLE(value, this.writePosition)
+    this.writePosition += 4
+  }
 
-		this.writePosition += 4
-	}
+  writeMultiByte(value, charSet = "utf8") {
+    const length = Buffer.byteLength(value)
 
-	writeInt(value) {
-		if (!this.canWrite(4)) {
-			this.scaleBuffer(4)
-		}
+    if (!this.canWrite(length)) {
+      this.scaleBuffer(length)
+    }
 
-		this.endian ? this.buffer.writeInt32BE(value, this.writePosition) : this.buffer.writeInt32LE(value, this.writePosition)
+    if (Buffer.isEncoding(charSet)) {
+      this.buffer.write(value, this.writePosition, length, charSet)
 
-		this.writePosition += 4
-	}
+      this.writePosition += length
+    }
+  }
 
-	writeMultiByte(value, charSet = "utf8") {
-		const length = Buffer.byteLength(value)
+  writeObject(value) {
+    this.objectEncoding === 3 ? new AMF3(this).writeData(value) : new AMF0(this).writeData(value)
+  }
 
-		if (!this.canWrite(length)) {
-			this.scaleBuffer(length)
-		}
+  writeShort(value) {
+    if (!this.canWrite(2)) {
+      this.scaleBuffer(2)
+    }
 
-		if (Buffer.isEncoding(charSet)) {
-			this.buffer.write(value, this.writePosition, length, charSet)
+    this.endian
+      ? this.buffer.writeInt16BE(value, this.writePosition)
+      : this.buffer.writeInt16LE(value, this.writePosition)
 
-			this.writePosition += length
-		}
-	}
+    this.writePosition += 2
+  }
 
-	writeObject(value) {
-		this.objectEncoding === 3 ? new AMF3(this).writeData(value) : new AMF0(this).writeData(value)
-	}
+  writeUnsignedByte(value) {
+    if (!this.canWrite(1)) {
+      this.scaleBuffer(1)
+    }
 
-	writeShort(value) {
-		if (!this.canWrite(2)) {
-			this.scaleBuffer(2)
-		}
+    this.buffer.writeUInt8(value, this.writePosition)
 
-		this.endian ? this.buffer.writeInt16BE(value, this.writePosition) : this.buffer.writeInt16LE(value, this.writePosition)
+    this.writePosition += 1
+  }
 
-		this.writePosition += 2
-	}
+  writeUnsignedInt(value) {
+    if (!this.canWrite(4)) {
+      this.scaleBuffer(4)
+    }
 
-	writeUnsignedByte(value) {
-		if (!this.canWrite(1)) {
-			this.scaleBuffer(1)
-		}
+    this.endian
+      ? this.buffer.writeUInt32BE(value, this.writePosition)
+      : this.buffer.writeUInt32LE(value, this.writePosition)
 
-		this.buffer.writeUInt8(value, this.writePosition)
+    this.writePosition += 4
+  }
 
-		this.writePosition += 1
-	}
+  writeUnsignedShort(value) {
+    if (!this.canWrite(2)) {
+      this.scaleBuffer(2)
+    }
 
-	writeUnsignedInt(value) {
-		if (!this.canWrite(4)) {
-			this.scaleBuffer(4)
-		}
+    this.endian
+      ? this.buffer.writeUInt16BE(value, this.writePosition)
+      : this.buffer.writeUInt16LE(value, this.writePosition)
 
-		this.endian ? this.buffer.writeUInt32BE(value, this.writePosition) : this.buffer.writeUInt32LE(value, this.writePosition)
+    this.writePosition += 2
+  }
 
-		this.writePosition += 4
-	}
+  writeUTF(value) {
+    const length = Buffer.byteLength(value)
 
-	writeUnsignedShort(value) {
-		if (!this.canWrite(2)) {
-			this.scaleBuffer(2)
-		}
+    if (length > 65535) {
+      throw new RangeError("Length can't be greater than 65535")
+    }
 
-		this.endian ? this.buffer.writeUInt16BE(value, this.writePosition) : this.buffer.writeUInt16LE(value, this.writePosition)
+    if (!this.canWrite(length)) {
+      this.scaleBuffer(length)
+    }
 
-		this.writePosition += 2
-	}
+    this.writeUnsignedShort(length)
 
-	writeUTF(value) {
-		const length = Buffer.byteLength(value)
+    this.buffer.write(value, this.writePosition, length)
 
-		if (!this.canWrite(length)) {
-			this.scaleBuffer(length)
-		}
+    this.writePosition += length
+  }
 
-		if (length > 65535) {
-			throw new RangeError("Length can't be greater than 65535")
-		}
-
-		this.writeUnsignedShort(length)
-
-		this.buffer.write(value, this.writePosition, length)
-
-		this.writePosition += length
-	}
-
-	writeUTFBytes(value) {
-		this.writeMultiByte(value)
-	}
+  writeUTFBytes(value) {
+    this.writeMultiByte(value)
+  }
 }
 
 module.exports = ByteArray
